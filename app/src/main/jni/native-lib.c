ab8af5b9c9592ee26cbf9f848b63d05b56ae63d2
davidglt <davidglt@hotmail.com>
2018-08-09 17:11:18 +0200

    jni deleted

--- app/src/main/jni/native-lib.c
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT void JNICALL
-Java_es_dragonit_opencvnativeandroidstudio_MainActivity_salt(JNIEnv *env, jobject instance,
-                                                             jlong matAddrGray, jint nbrElem) {
-
-    // TODO
-
-}
